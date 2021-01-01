c86dc38bb0affd961ea952172e6fb8605d415b40
Lukas Böger <boeger@plexim.com>
2021-01-01 18:51:57 +0000

    Use Boost's multiprecision instead of rational

--- src/rational.h
+++ src/rational.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <boost/rational.hpp>
 #include "int.h"
 
 namespace sym2 {
-    using Rational = boost::rational<Int>;
+    using Rational = boost::multiprecision::number<
+      boost::multiprecision::rational_adaptor<boost::multiprecision::cpp_int_backend<integerBitBufferSize>>>;
 }
