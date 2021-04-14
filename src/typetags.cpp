8396f667754a2d9d97e52d09a384f486c4b113bf
Lukas Böger <mail@lboeger.de>
2021-04-14 22:05:12 +0100

    Forward type-tag based predicates to new ones

--- src/typetags.cpp
+++ src/typetags.cpp
@@ -1,147 +1,100 @@
 
 #include "typetags.h"
+#include "predicates.h"
 #include "query.h"
 
 template <>
 bool sym2::is<sym2::Real>(ExprView e)
 {
-    // TODO This is incomplete, we need a tag for symbols to be marked real
-    switch (type(e)) {
-        case Type::smallInt:
-        case Type::smallRational:
-        case Type::floatingPoint:
-        case Type::largeInt:
-        case Type::largeRational:
-            return true;
-        default:
-            return false;
-    }
+    return isRealDomain(e);
 }
 
 template <>
 bool sym2::is<sym2::Complex>(ExprView e)
 {
-    // TODO This is incomplete, we need a tag for symbols to be marked complex
-    return type(e) == Type::complexNumber;
+    return isComplexDomain(e);
 }
 
 template <>
 bool sym2::is<sym2::Number>(ExprView e)
 {
-    switch (type(e)) {
-        case Type::smallInt:
-        case Type::smallRational:
-        case Type::floatingPoint:
-        case Type::largeInt:
-        case Type::largeRational:
-        case Type::complexNumber:
-            return true;
-        default:
-            return false;
-    }
+    return isNumber(e);
 }
 
 template <>
 bool sym2::is<sym2::Int>(ExprView e)
 {
-    const Type t = type(e);
-
-    return t == Type::smallInt || t == Type::largeInt;
+    return isInteger(e);
 }
 
 template <>
 bool sym2::is<sym2::Rational>(ExprView e)
 {
-    switch (type(e)) {
-        case Type::smallInt:
-        case Type::smallRational:
-        case Type::largeInt:
-        case Type::largeRational:
-            return true;
-        default:
-            return false;
-    }
+    return isRational(e);
 }
 
 template <>
 bool sym2::is<sym2::Double>(ExprView e)
 {
-    return type(e) == Type::floatingPoint;
+    return isFloatingPoint(e);
 }
 
 template <>
 bool sym2::is<sym2::Small>(ExprView e)
 {
-    switch (type(e)) {
-        case Type::smallInt:
-        case Type::smallRational:
-        case Type::symbol:
-        case Type::floatingPoint:
-        case Type::constant:
-            return true;
-        default:
-            return false;
-    }
+    return isSmall(e);
 }
 
 template <>
 bool sym2::is<sym2::Large>(ExprView e)
 {
-    return !is<Small>(e);
+    return isLarge(e);
 }
 
 template <>
 bool sym2::is<sym2::Scalar>(ExprView e)
 {
-    switch (type(e)) {
-        case Type::sum:
-        case Type::product:
-        case Type::power:
-        case Type::function:
-            return false;
-        default:
-            return true;
-    }
+    return isScalar(e);
 }
 
 template <>
 bool sym2::is<sym2::Symbol>(ExprView e)
 {
-    return type(e) == Type::symbol;
+    return isSymbol(e);
 }
 
 template <>
 bool sym2::is<sym2::Constant>(ExprView e)
 {
-    return type(e) == Type::constant;
+    return isConstant(e);
 }
 
 template <>
 bool sym2::is<sym2::Sum>(ExprView e)
 {
-    return type(e) == Type::sum;
+    return isSum(e);
 }
 
 template <>
 bool sym2::is<sym2::Product>(ExprView e)
 {
-    return type(e) == Type::product;
+    return isProduct(e);
 }
 
 template <>
 bool sym2::is<sym2::Power>(ExprView e)
 {
-    return type(e) == Type::power;
+    return isPower(e);
 }
 
 template <>
 bool sym2::is<sym2::Function>(ExprView e)
 {
-    return type(e) == Type::function;
+    return isFunction(e);
 }
 
 template <>
 bool sym2::is<sym2::NumericallyEvaluable>(ExprView e)
 {
-    return (flags(e) & Flag::numericallyEvaluable) != Flag::none;
+    return isNumericallyEvaluable(e);
 }
