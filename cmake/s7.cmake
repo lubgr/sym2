2f58d3b11289b9d15698ec1ee2893e17b6bcb757
Lukas Böger <mail@lboeger.de>
2021-07-22 23:22:23 +0100

    Compile s7 with WITH_HISTORY flag

--- cmake/s7.cmake
+++ cmake/s7.cmake
@@ -5,6 +5,10 @@ set(s7BinaryDir ${sym2_BINARY_DIR}/lib/s7)
 add_library(s7
     ${s7SourceDir}/s7.c)
 
+target_compile_definitions(s7
+    PRIVATE
+    WITH_HISTORY=1)
+
 file(TOUCH
     ${s7BinaryDir}/mus-config.h)
 
