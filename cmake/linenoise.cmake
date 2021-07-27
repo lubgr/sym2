3c4a58e9b4458921fe1509a0976f591b68308ecd
Lukas Böger <mail@lboeger.de>
2021-07-27 23:07:35 +0100

    Remove s7 bindings attempt

--- cmake/linenoise.cmake
+++
@@ -1,9 +0,0 @@
-
-set(linenoiseSourceDir ${sym2_SOURCE_DIR}/lib/linenoise)
-
-add_library(linenoise
-    ${linenoiseSourceDir}/linenoise.c)
-
-target_include_directories(linenoise
-    PUBLIC
-    ${linenoiseSourceDir})
