77ee32e20aadc6fa2b3b5eed0c7fb1368bc030d6
Lukas Böger <mail@lboeger.de>
2021-07-28 11:45:46 +0100

    Add license header

--- src/stdbackport.h
+++ src/stdbackport.h
@@ -3,6 +3,12 @@
 #include <concepts>
 #include <type_traits>
 
+/* Adjusted from LLVM, file libcxx/include/concepts, see the LICENSE file of sym2 for more details and a copy of the
+ * license.
+ *
+ * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions. See https://llvm.org/LICENSE.txt for
+ * license information. SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception */
+
 namespace stdbackport {
     template <class _From, class _To>
     concept convertible_to = std::is_convertible_v<_From, _To> && requires(std::add_rvalue_reference_t<_From> (&__f)())
